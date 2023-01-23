9577a2a0aea95b98b5177321c5c6995b2f678457
Deepstrats <you@example.com>
2023-01-23 12:18:46 +0530

    saved

--- ex1.py
+++ ex1.py
@@ -1,4 +1,4 @@
-Org = "Deloitte_blr"
+Org = "Deloitte_bellandur"
 ID = "Batch_A"
-year = 2023_Jan
+year = 2023_Jan-Mar
 print("Welcome to the CDF conducted by {} in the year of {} and we are in {}".format(Org,year,ID))
